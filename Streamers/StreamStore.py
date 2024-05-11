15f7330a9ad9a5b00a392fd7cebde52a1834263a
IornBird <iornbird14142@gmail.com>
2024-05-11 23:00:59 +0800

    fix errors that made my modify on morning thrown into ditch

--- Streamers/StreamStore.py
+++ Streamers/StreamStore.py
@@ -27,27 +27,25 @@ class StreamStore:
         """
         print("record")
         try:
-            capture = cv2.VideoCapture(self.streamSrc)
-            print("capture set for", self.file)
-            # capture.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
-            # capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
-            out = cv2.VideoWriter(self.file, cv2.VideoWriter.fourcc(*'XVID'), self.fps, (320, 240))
+            cap = cv2.VideoCapture(self.streamSrc)
+            fourcc = cv2.VideoWriter.fourcc(*'XVID')
+            out = cv2.VideoWriter(self.file, fourcc, self.fps, (640, 480))
             start_time = time.time()
+            frmNum = 0
             print('loop')
             while True:
-                ret, frame = capture.read()
+                ret, frame = cap.read()
                 if ret:
-                    out.write(frame)
-                    self.shared_Frame.__setitem__(0, frame)
-                    print("\twritten")
-                if time.time() - start_time >= 5:# or self.doRelease:
-                    capture.release()
-                    out.release()
-                    cv2.destroyAllWindows()
-                    print("return")
-                    return
-                    # out = cv2.VideoWriter(self.file, cv2.VideoWriter.fourcc(*'mp4v'), self.fps, (320, 240))
-                    # start_time = time.time()
+                    while frmNum < (time.time() - start_time) * self.fps:
+                        out.write(frame)
+                        frmNum += 1
+                    if time.time() - start_time >= 5:
+                        out.release()
+                        cap.release()
+                        cv2.destroyAllWindows()
+                        break
+                else:
+                    break
         except Exception as e:
             print("Exception")
             self.running = False
@@ -93,19 +91,51 @@ def sleep(t):
         pass
 
 
+FILE = '../videos/0.avi'
+
+def record():
+    cap = cv2.VideoCapture(0)
+    fourcc = cv2.VideoWriter.fourcc(*'XVID')
+    out = cv2.VideoWriter(FILE, fourcc, 60.0, (640, 480))
+    start_time = time.time()
+    frmNum = 0
+    print('loop')
+    while True:
+        ret, frame = cap.read()
+        if ret:
+            while frmNum < (time.time() - start_time) * 60.0:
+                out.write(frame)
+                frmNum += 1
+            if time.time() - start_time >= 5:
+                out.release()
+                cap.release()
+                cv2.destroyAllWindows()
+                break
+        else:
+            break
+
+
+def canRead():
+    cap = cv2.VideoCapture(FILE)
+    ret, frame = cap.read()
+    cap.release()
+    return ret
+
+_record_test = False
 if __name__ == '__main__':
-    f = open('../videos/0.avi', 'rt')
-    print(f.read())
-    # f.write(bytes([97, 98, 99]))
+    f = open(FILE, 'rb')
+    print(f.read()[:10])
     f.close()
     input("Press [Enter] to continue")
-    ss = StreamStore(0, '../videos/0.avi')
-    print("play starts")
-    #ss.Play()
-    ss.record_frames()
-    print("end")
-    # print("wait for 6 sec")
-    # sleep(10)
-    # print("pause")
-    # ss.Pause()
-    # print("end")
+    if _record_test:
+        record()
+        print("check if record successes")
+        print(canRead())
+        print("end")
+    else:
+        ss = StreamStore(0, FILE)
+        print("record")
+        ss.Play()
+        print("check if record in another process successes")
+        print(canRead())
+        print("end")
